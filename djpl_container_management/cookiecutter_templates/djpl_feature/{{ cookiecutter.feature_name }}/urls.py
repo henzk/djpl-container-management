5f48555a8253a21468a89776463c4a0c8a3fbbec
Hendrik Speidel <speidel@schnapptack.de>
2014-04-26 20:13:23 +0200

    added example urls to djpl_feature cookiecutter

--- djpl_container_management/cookiecutter_templates/djpl_feature/{{ cookiecutter.feature_name }}/urls.py
+++ djpl_container_management/cookiecutter_templates/djpl_feature/{{ cookiecutter.feature_name }}/urls.py
@@ -1,12 +1,16 @@
 
 
-
 def refine_get_urls(original):
 
     def get_urls():
         from django.conf.urls import url, patterns, include
         urlpatterns = patterns('',
             # place your feature's urls here
+            # see: https://docs.djangoproject.com/en/dev/topics/http/urls/
+            # examples:
+            # url(r'^articles/([0-9]{4})/([0-9]{2})/([0-9]+)/$', 'news.views.article_detail'),
+            # url(r'^(?P<username>\w+)/blog/', include('foo.urls.blog')),
+
         )
         return urlpatterns + original()
     return get_urls
