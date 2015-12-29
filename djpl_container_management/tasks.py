646de4850ccd2fa707d6bab3ad371762ad107d29
Michael Galler <michael.galler@schnapptack.de>
2015-12-29 16:17:38 +0100

    added create_project_from task - copys whole container with some excludes

--- djpl_container_management/tasks.py
+++ djpl_container_management/tasks.py
@@ -2,6 +2,7 @@ from ape import tasks
 import os
 import json
 import sys
+import shutil
 
 @tasks.register_helper
 def get_cookiecutter_template_dir(template_name):
@@ -135,3 +136,51 @@ def create_feature(feature_name, container_name=None, location=None):
         output_dir=location
     )
     print '*** Created feature %s in %s' % (feature_name, location)
+
+
+def get_containers_location(poi):
+    parts = poi.split(':')
+    if len(parts) == 2:
+        source_container, target_container = parts[0], parts[1]
+        return (source_container, target_container)
+    else:
+        raise Exception('unable to parse context - format: <source_container>:<target_container>')
+
+@tasks.register
+def create_project_from(poi, exclude=''):
+    '''
+    This task copy a whole container except for _lib in container root, context.json and __data__ in all products and additionally excludes.
+    The argument <poi> is in the form source_container:target_container
+    The optional argument <exclude> is in the form a,b,c - seperate exclude files and dirs with ,
+    '''
+    apedir = os.environ['APE_ROOT_DIR']
+    source_container, target_container = get_containers_location(poi)
+
+    source_container_dir = '%s/%s' % (apedir, source_container)
+    target_container_dir = '%s/%s' % (apedir, target_container)
+    if not os.path.isdir(source_container_dir):
+        print 'ERROR: %s is not a valid container as it does not exist.' % source_container_dir
+        return
+
+    products_dir = source_container_dir + '/products'
+    if not os.path.isdir(products_dir):
+        print 'ERROR: %s must contain a "products" directory.' % source_container_dir
+        return
+
+    if os.path.isdir(target_container_dir):
+        print 'ERROR: Target Container %s already exist' % target_container_dir
+        return
+
+
+    exclude_list = ['_lib', '__data__', 'context.json', 'httpd'] + exclude.split(',')
+
+    print 'copy from %s to %s ..... ' % (source_container_dir, target_container_dir)
+    try:
+        shutil.copytree(source_container_dir, target_container_dir, ignore=shutil.ignore_patterns(*exclude_list))
+    except OSError as e:
+        if e.errno == errno.ENOTDIR:
+            shutil.copy(source_container_dir, target_container_dir)
+        else:
+            print('ERROR: Directory not copied. Error: %s' % e)
+            return
+    print 'done - now start with ape install_cotainer %s' %  target_container
